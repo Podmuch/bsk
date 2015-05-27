ad262871b9941a1fbaec10718135deb3beaa1a26
Dominik <dominikpielak@gmail.com>
2015-05-27 18:39:24 +0200

    https
    jeden krok do pokonania PS blizej

--- BSK/klientwebowy/App_Start/FilterConfig.cs
+++ BSK/klientwebowy/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace klientwebowy
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
